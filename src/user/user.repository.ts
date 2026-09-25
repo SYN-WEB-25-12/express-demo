2b045a947db7346a1afab8a3e92236631237bbcc
Nick Podratz <nick.podratz@syntax-institut.de>
2026-09-25 12:10:43 +0200

    Delay row accessing

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,17 +1,17 @@
 import type { User } from "./user.types.js";
 import { getPostgresPool } from "../db/config.postgres.js";
 import { PG_ERROR, UniqueConstraintViolated } from "./user.errors.js";
+import type { QueryResult } from "pg";
 
 const pool = getPostgresPool()
 
 async function createUser(username: string): Promise<User> {
     const sql = "INSERT INTO users (username) VALUES ($1) RETURNING id, username;"
 
-    let user: User
+    let result: QueryResult<User>
 
     try {
-        const result = await pool.query<User>(sql, [username])
-        user = result.rows[0]!
+        result = await pool.query(sql, [username])
     } catch (err) {
         if (err instanceof Error && "code" in err && err.code == PG_ERROR.UNIQUE_CONSTRAINT_VIOLATED) {
             throw new UniqueConstraintViolated("username", username)
@@ -20,7 +20,7 @@ async function createUser(username: string): Promise<User> {
         throw err
     }
     
-    return user
+    return result.rows[0]! // Always returns one row via RETURNING ...
 }
 
 async function getAllUsers(): Promise<User[]> {
